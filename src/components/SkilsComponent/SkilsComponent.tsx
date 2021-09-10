26e4274cb83899ad9ae3cb6667656370a73277ad
hkiko <hristijanpetkovski@hotmail.com>
2021-09-10 12:32:53 +0200

    fix

--- src/components/SkilsComponent/SkilsComponent.tsx
+++ src/components/SkilsComponent/SkilsComponent.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import arrowIcon from "../../assets/icon/arrow-platform-icon.svg";
+import Fade from "react-reveal/Fade";
 
 import "./SkilsComponent.scss";
 
@@ -10,37 +11,47 @@ type SkilsProps = {
 export const SkilsComponent: React.FC<SkilsProps> = ({ listOfJobs }) => {
   return (
     <div className=" row item-conatiner">
+    
       <div className="col-md-3 item-list-top">
         {listOfJobs.slice(0, 7).map((item, index) => (
+            <Fade top>
           <p className="title-items" key={index}>
             <img className="list-img" src={arrowIcon} alt="proffesion" />{" "}
             {item.job}
           </p>
+          </Fade>
         ))}
       </div>
+
       <div className="col-md-3  item-list">
         {listOfJobs.slice(7, 14).map((item, index) => (
+          <Fade top>
           <p className="title-items" key={index}>
             <img className="list-img" src={arrowIcon} alt="proffesion" />{" "}
             {item.job}
           </p>
+          </Fade>
         ))}
       </div>
 
       <div className="col-md-3  item-list">
         {listOfJobs.slice(14, 21).map((item, index) => (
+          <Fade top>
           <p className="title-items" key={index}>
             <img className="list-img" src={arrowIcon} alt="proffesion" />{" "}
             {item.job}
           </p>
+          </Fade>
         ))}
       </div>
       <div className="col-md-3  item-list-bottom">
         {listOfJobs.slice(21, 28).map((item, index) => (
+          <Fade top>
           <p className="title-items" key={index}>
             <img className="list-img" src={arrowIcon} alt="proffesion" />{" "}
             {item.job}
           </p>
+          </Fade>
         ))}
       </div>
     </div>
