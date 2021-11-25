7ae8ed04b74b27d5a60c27cc76cf3979e10a69d0
hkiko <hristijanpetkovski@hotmail.com>
2021-11-25 18:50:32 +0100

    fix ga

--- src/index.tsx
+++ src/index.tsx
@@ -1,41 +1,43 @@
-import ReactDOM from 'react-dom';
-import { createBrowserHistory } from 'history';
-import { Router, Switch } from 'react-router-dom';
-import { configureStore } from './store';
-import ReactGA from 'react-ga';
+import ReactDOM from "react-dom";
+import { createBrowserHistory } from "history";
+import { Router, Switch } from "react-router-dom";
+import { configureStore } from "./store";
+import ReactGA from "react-ga";
+import { withRouter } from "react-router-dom";
+import { RouteSwitcher } from "components/RouterSwitcher/RouteSwitcher";
 
-import { RouteSwitcher } from 'components/RouterSwitcher/RouteSwitcher';
+import "./i18n";
+import "./index.scss";
 
-import './i18n';
-import './index.scss';
-;
-const trackingId = "G-SC3JZRM40X"; // Replace with your Google Analytics tracking ID
-ReactGA.initialize(trackingId);
+const TRACKING_ID = "G-SC3JZRM40X"; // YOUR_OWN_TRACKING_ID
+ReactGA.initialize(TRACKING_ID);
 
 const hist = createBrowserHistory();
 const store = configureStore();
 
-// hist.listen(location => {
-//   ReactGA.set({ page: location.pathname }); // Update the user's current page
-//   ReactGA.pageview(location.pathname); // Record a pageview for the given page
-// });
-
 ReactDOM.render(
+  <Router history={hist}>
+    <link
+      rel="stylesheet"
+      href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+    />
+    <Switch>
+      <RouteSwitcher path="/" />
+    </Switch>
+  </Router>,
 
-    <Router history={hist}>
-      <link
-        rel="stylesheet"
-        href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
-      />
-      <Switch>
-        <RouteSwitcher path="/" />
-      </Switch>
-    </Router>
-    
-    ,
-  
-  document.getElementById('root')
+  document.getElementById("root")
 );
-ReactGA.initialize("G-SC3JZRM40X");
-ReactGA.pageview(window.location.pathname + window.location.search);
+
 export const history = hist;
+
+const RouteChangeTracker = ({ hist }) => {
+  hist.listen((location, action) => {
+    ReactGA.set({ page: location.pathname });
+    ReactGA.pageview(location.pathname);
+  });
+
+  return
+};
+
+export default withRouter(RouteChangeTracker);
