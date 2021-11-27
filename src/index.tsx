7c72430a58f88cec44956a2f567d6a4eb11884b0
hkiko <hristijanpetkovski@hotmail.com>
2021-11-27 16:52:14 +0100

    fix ga

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,16 @@
+import { useEffect } from "react";
 import ReactDOM from "react-dom";
 import { createBrowserHistory } from "history";
 import { Router, Switch } from "react-router-dom";
 import { configureStore } from "./store";
 import ReactGA from "react-ga";
-import { withRouter } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import { RouteSwitcher } from "components/RouterSwitcher/RouteSwitcher";
 
 import "./i18n";
 import "./index.scss";
 
-const TRACKING_ID = "G-SC3JZRM40X"; // YOUR_OWN_TRACKING_ID
-ReactGA.initialize(TRACKING_ID);
+
 
 const hist = createBrowserHistory();
 const store = configureStore();
@@ -21,23 +21,27 @@ ReactDOM.render(
       rel="stylesheet"
       href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
     />
+     <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-SC3JZRM40X"
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-SC3JZRM40X');
+              `,
+            }}
+          />
     <Switch>
       <RouteSwitcher path="/" />
     </Switch>
-  </Router>,
+  </Router>
+,
 
-  document.getElementById("root")
+  document.getElementById("root"),
 );
 
-export const history = hist;
 
-const RouteChangeTracker = ({ hist }) => {
-  hist.listen((location, action) => {
-    ReactGA.set({ page: location.pathname });
-    ReactGA.pageview(location.pathname);
-  });
-
-  return
-};
-
-export default withRouter(RouteChangeTracker);
