ebdb0c563973a6634fbd9044a7ed4ae1b94bb559
CLSDevelopment <88430298+CLSDevelopment@users.noreply.github.com>
2021-11-30 12:52:56 +0100

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -9,16 +9,7 @@ import "./index.scss";
 
 const history = createBrowserHistory();
 
-const GAImplementation = `
-<script type="text/javascript">
-(function(window, document, dataLayerName, id) {
-function stgCreateCookie(a,b,c){var d="";if(c){var e=new Date;e.setTime(e.getTime()+24*c*60*60*1e3),d="; expires="+e.toUTCString()}document.cookie=a+"="+b+d+"; path=/"}
-var isStgDebug=(window.location.href.match("stg_debug")||document.cookie.match("stg_debug"))&&!window.location.href.match("stg_disable_debug");stgCreateCookie("stg_debug",isStgDebug?1:"",isStgDebug?14:-1);
-var qP=[];dataLayerName!=="dataLayer"&&qP.push("data_layer_name="+dataLayerName),isStgDebug&&qP.push("stg_debug");var qPString=qP.length>0?("?"+qP.join("&")):"";
-document.write('<script src="https://clearsightfinance.containers.piwik.pro/'+id+'.sync.js' + qPString + '"></' + 'script>');
-})(window, document, 'dataLayer', '678e8667-357e-4024-b795-2df0248a71d2');
-</script>
-`;
+
 
 const iubendaImplementation = `
 <script type="text/javascript">
@@ -41,7 +32,7 @@ ReactDOM.render(
   
     
     </Helmet>
-   <div dangerouslySetInnerHTML={{ __html: GAImplementation }}></div>
+
     <div dangerouslySetInnerHTML={{ __html: iubendaImplementation }}></div>
     <Switch>
       <RouteSwitcher path="/" />
