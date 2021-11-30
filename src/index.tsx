c28c012667fbe2aae6d65fc5f0ed37705f4d2d90
CLSDevelopment <88430298+CLSDevelopment@users.noreply.github.com>
2021-11-30 12:25:40 +0100

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -9,14 +9,7 @@ import "./index.scss";
 
 const history = createBrowserHistory();
 
-const iubendaImplementation = `
-<script type="text/javascript">
-var _iub = _iub || [];
-_iub.csConfiguration = {"ccpaAcknowledgeOnDisplay":true,"whitelabel":false,"lang":"en","siteId":2315787,"enableCcpa":true,"countryDetection":true,"enableCMP":true,"googleAdditionalConsentMode":true,"cookiePolicyId":49402867, "banner":{ "acceptButtonDisplay":true,"customizeButtonDisplay":true,"position":"float-bottom-center","acceptButtonColor":"#0073CE","acceptButtonCaptionColor":"white","customizeButtonColor":"#f14e22","customizeButtonCaptionColor":"white","textColor":"#010000","backgroundColor":"#ffffff","rejectButtonDisplay":true,"rejectButtonColor":"#0073CE","rejectButtonCaptionColor":"white" }};
-</script>
-<script type="text/javascript" src="//cdn.iubenda.com/cs/tcf/stub-v2.js"></script>
-<script type="text/javascript" src="//cdn.iubenda.com/cs/ccpa/stub.js"></script>
-<script type="text/javascript" src="//cdn.iubenda.com/cs/iubenda_cs.js" charset="UTF-8" async></script>
+const GAImplementation = `
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-SC3JZRM40X"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
@@ -27,6 +20,17 @@ _iub.csConfiguration = {"ccpaAcknowledgeOnDisplay":true,"whitelabel":false,"lang
 </script>
 `;
 
+const iubendaImplementation = `
+<script type="text/javascript">
+var _iub = _iub || [];
+_iub.csConfiguration = {"ccpaAcknowledgeOnDisplay":true,"whitelabel":false,"lang":"en","siteId":2315787,"enableCcpa":true,"countryDetection":true,"enableCMP":true,"googleAdditionalConsentMode":true,"cookiePolicyId":49402867, "banner":{ "acceptButtonDisplay":true,"customizeButtonDisplay":true,"position":"float-bottom-center","acceptButtonColor":"#0073CE","acceptButtonCaptionColor":"white","customizeButtonColor":"#f14e22","customizeButtonCaptionColor":"white","textColor":"#010000","backgroundColor":"#ffffff","rejectButtonDisplay":true,"rejectButtonColor":"#0073CE","rejectButtonCaptionColor":"white" }};
+</script>
+<script type="text/javascript" src="//cdn.iubenda.com/cs/tcf/stub-v2.js"></script>
+<script type="text/javascript" src="//cdn.iubenda.com/cs/ccpa/stub.js"></script>
+<script type="text/javascript" src="//cdn.iubenda.com/cs/iubenda_cs.js" charset="UTF-8" async></script>
+
+`;
+
 ReactDOM.render(
   <Router history={history}>
     <Helmet>
@@ -34,7 +38,7 @@ ReactDOM.render(
         rel="stylesheet"
         href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
       />
-     
+     <div dangerouslySetInnerHTML={{ __html: GAImplementation }}></div>
     
     </Helmet>
 
