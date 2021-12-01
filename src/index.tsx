722001c32a6582dabbda4717dea17b870e1c3983
CLSDevelopment <88430298+CLSDevelopment@users.noreply.github.com>
2021-12-01 17:00:20 +0100

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -10,13 +10,14 @@ import "./index.scss";
 const history = createBrowserHistory();
 
 const GAImplementation = `
-  <script async src="https://www.googletagmanager.com/gtag/js?id=G-N76KX1TJ8N"></script>
 <script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-  gtag('config', 'G-N76KX1TJ8N');
+ga('create', 'G-SC3JZRM40X', 'auto');
+ga('send', 'pageview');
 </script>
 `;
 
